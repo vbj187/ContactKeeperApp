202017f0a56da64f7d96347af8ef2642f89ab3e7
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-14 21:31:21 +0530

    implemented bcrypt-js for register route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -17,12 +18,37 @@ router.post(
       'Please enter a password with six or more character'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.json({ message: 'passed' });
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('user saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
