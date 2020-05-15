c908012a8d1eaac7e8e84d51c26e7eb2568b966e
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-15 19:00:01 +0530

    contacts objects send to specific user after auth with jwt

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-// @route  "GET api/contacts"
-// @description "GET all users contacts separately"
-// @access "Private"
-router.get('/', (req, res) => {
-  res.send('get all contacts');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+
+// @route     GET api/contacts
+// @desc      Get all users contacts
+// @access    Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route  "POST api/contacts"
