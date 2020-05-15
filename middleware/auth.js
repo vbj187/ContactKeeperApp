6f0c6bc5d769b7e07cb9ec809690dc483b21fed6
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-15 20:22:06 +0530

    add contacts route created and checked for getting specified contacts for specific users

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,16 +5,17 @@ module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if no token
+  // Check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No Token, Authorization denied' });
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
+
     req.user = decoded.user;
     next();
-  } catch (error) {
+  } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
