a5c488dfef711797ccd919a1bf914c90d1a2357b
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-15 12:24:18 +0530

    user object send via auth in protected route

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,14 @@ const User = require('../models/User');
 // @route  GET api/auth
 // @description GET logged in user
 // @access Private
-router.get('/', auth, (req, res) => {
-  res.send('get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route  POST api/auth
