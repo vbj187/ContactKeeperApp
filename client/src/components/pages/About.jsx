bd0380ac563571b9180d4b5c3ee115fb0ba934ce
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-17 11:10:30 +0530

    created contactsFilter component and added functionality

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h1>About this App</h1>
+      <h1>About</h1>
       <p className='my-1'>
         This is a full stack React app for keeping contacts
       </p>
+      <p className='my-1'>Created by Vigneshwaran</p>
       <p className='bg-dark p'>
         <strong>Version:</strong> 1.0.0
       </p>
