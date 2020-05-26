ad276900e4401cb266a187c004d0934988cb499d
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-26 22:43:12 +0530

    deleteContact context implemented & checked

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -64,8 +64,20 @@ const ContactState = (props) => {
   };
 
   // Delete contact
-  const deleteContact = (id) => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async (id) => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
   };
 
   // Clear Contacts
