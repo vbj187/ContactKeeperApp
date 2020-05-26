cae30ab777638c77b1f9d16fce71f1368df212b8
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-26 22:51:16 +0530

    updateContact state created

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -80,6 +80,32 @@ const ContactState = (props) => {
     }
   };
 
+  // update contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
+
   // Clear Contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -93,11 +119,6 @@ const ContactState = (props) => {
   // clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // update contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // filter contact
